e0694a995e9f62a913ef2909d5106c8f6bdaef6c
Eng_Mo27 <139957142+mohamedf1234@users.noreply.github.com>
2026-05-14 21:01:32 +0300

    Add newline at end of __init__.py
    Added a newline at the end of the file for consistency.

--- controllers/__init__.py
+++ controllers/__init__.py
@@ -2,6 +2,7 @@ from .mode_controller import get_active_mode, set_active_mode
 from .inference_controller import process_uploaded_frame, get_latest_results
 from .system_controller import health_check, reload_model, start_retrain_process
 
+
 def register_routes(app):
     from routes import register_routes as register_all_routes
     register_all_routes(app)
