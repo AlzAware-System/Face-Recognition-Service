d05035e9e0d9e8d9fea5ca58e8f7733f4d3634dc
mohamed ashraf <mohamed.ashraf.w12345@gmail.com>
2026-07-01 03:10:36 +0300

    fix: remove unused load_yolo_general_model imports

--- Face-Recognition-Server/services/__init__.py
+++ Face-Recognition-Server/services/__init__.py
@@ -1,10 +1,8 @@
 from .model_loader import (
     load_model_from_s3,
     load_yolo_model,
-    load_yolo_general_model,
     is_svm_model_loaded,
     is_yolo_model_loaded,
-    is_yolo_general_model_loaded,
 )
 from .prediction_service import predict_face, predict_medicine
 from .state_store import RAW_FRAME_BUFFER, CURRENT_FRAME_RESULT, buffer_lock, frame_processed_event
@@ -13,10 +11,8 @@ from .worker_service import analysis_worker, start_analysis_worker
 __all__ = [
     "load_model_from_s3",
     "load_yolo_model",
-    "load_yolo_general_model",
     "is_svm_model_loaded",
     "is_yolo_model_loaded",
-    "is_yolo_general_model_loaded",
     "predict_face",
     "predict_medicine",
     "RAW_FRAME_BUFFER",
